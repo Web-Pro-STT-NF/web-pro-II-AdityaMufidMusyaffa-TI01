1f56857334259b8ade481d83b06a7cb1f33bc493
adityamusyaffa <adit21030ti@student.nurulfikri.ac.id>
2022-03-18 00:48:04 +0700

    write variable user

---
+++ praktikum-1/1-variable/2-variable-user.php
@@ -0,0 +1,13 @@
+<?php
+// mendefinisikan variables user
+$nama = 'Aditya Mufid Musyaffa';
+$umur = 19;
+$berat = 51.7;
+
+echo 'Nama : ' . $nama;
+echo '<br> Umur : ' . $umur.' Tahun';
+echo '<br> Berat : '.$berat.' Kg';
+
+echo "<br> Hello $nama Apakabar";
+
+?>
