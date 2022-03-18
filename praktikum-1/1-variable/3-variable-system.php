415ae2ed9e8dead46e6d69b5fefc1e92a44a2d4b
adityamusyaffa <adit21030ti@student.nurulfikri.ac.id>
2022-03-19 00:10:50 +0700

    write 3-variable-system.php

---
+++ praktikum-1/1-variable/3-variable-system.php
@@ -0,0 +1,9 @@
+<?php
+// variable system
+print_r($_SERVER);
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo 'Dokumen Root: '.$_SERVER["DOCUMENT_ROOT"];
+echo '<br/>Nama File: '.$_SERVER["PHP_SELF"];
+?>
