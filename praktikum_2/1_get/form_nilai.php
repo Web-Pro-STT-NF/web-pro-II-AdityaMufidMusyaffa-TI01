902b721dbee4e53f34f724b79fb0251ebe7d8a24
adityamusyaffa <adit21030ti@student.nurulfikri.ac.id>
2022-03-29 20:36:46 +0700

    Add header and footer bootstrap

--- praktikum_2/1_get/form_nilai.php
+++ praktikum_2/1_get/form_nilai.php
@@ -5,56 +5,69 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
   <title>Form nilai</title>
 </head>
 <body>
-<div class="container mt-3">
-<legend><h2>Form Nilai Siswa</h2></legend>
-<hr style="border-bottom: 5px solid black;">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 bg-light pt-2 border border-2">
+        <p>Sistem Penilaian</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h4 class="pt-2">Form Nilai Siswa</h4>
+        <hr>
+      </div>
+    </div>
+  </div>
 
-  <form action="" method="GET">
-    <div class="form-group row">
-      <label class="col-4 col-form-label" for="nama">Nama Lengkap</label> 
-      <div class="col-8">
-        <input id="nama" name="nama" placeholder="Nama Lengkap" type="text" required="required" class="form-control">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <form action="" method="GET">
+          <div class="form-group row">
+            <label class="col-4 col-form-label" for="nama">Nama Lengkap</label> 
+            <div class="col-8">
+              <input id="nama" name="nama" placeholder="Nama Lengkap" type="text" required="required" class="form-control">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="matkul" class="col-4 col-form-label">Mata Kuliah</label> 
+            <div class="col-8">
+              <select id="matkul" name="matkul" class="custom-select" required="required">
+                <option value="DDP" name="ddp">Dasa Dasar Pemrograman</option>
+                <option value="UI/UX">UI/UX</option>
+                <option value="Pemrograman Web">Pemrograman Web</option>
+              </select>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="nilaiuts" class="col-4 col-form-label">Nilai UTS</label> 
+            <div class="col-8">
+              <input id="nilaiuts" name="nilaiuts" placeholder="Nilai UTS" type="text" class="form-control">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="nilaiuas" class="col-4 col-form-label">Nilai UAS</label> 
+            <div class="col-8">
+              <input id="nilaiuas" name="nilaiuas" placeholder="Nilai UAS" type="text" class="form-control">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="nilaiTugas" class="col-4 col-form-label">Nilai Tugas / Praktikum</label> 
+            <div class="col-8">
+              <input id="nilaitugas" name="nilaitugas" placeholder="Nilai Tugas" type="text" class="form-control">
+            </div>
+          </div> 
+          <div class="form-group row">
+            <div class="offset-4 col-8">
+              <button name="submit" type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
-    <div class="form-group row">
-      <label for="matkul" class="col-4 col-form-label">Mata Kuliah</label> 
-      <div class="col-8">
-        <select id="matkul" name="matkul" class="custom-select" required="required">
-          <option value="DDP" name="ddp">Dasa Dasar Pemrograman</option>
-          <option value="UI/UX">UI/UX</option>
-          <option value="Pemrograman Web">Pemrograman Web</option>
-        </select>
-      </div>
-    </div>
-    <div class="form-group row">
-      <label for="nilaiuts" class="col-4 col-form-label">Nilai UTS</label> 
-      <div class="col-8">
-        <input id="nilaiuts" name="nilaiuts" placeholder="Nilai UTS" type="text" class="form-control">
-      </div>
-    </div>
-    <div class="form-group row">
-      <label for="nilaiuas" class="col-4 col-form-label">Nilai UAS</label> 
-      <div class="col-8">
-        <input id="nilaiuas" name="nilaiuas" placeholder="Nilai UAS" type="text" class="form-control">
-      </div>
-    </div>
-    <div class="form-group row">
-      <label for="nilaiTugas" class="col-4 col-form-label">Nilai Tugas / Praktikum</label> 
-      <div class="col-8">
-        <input id="nilaitugas" name="nilaitugas" placeholder="Nilai Tugas" type="text" class="form-control">
-      </div>
-    </div> 
-    <div class="form-group row">
-      <div class="offset-4 col-8">
-        <button name="submit" type="submit" class="btn btn-primary">Simpan</button>
-      </div>
-    </div>
-  </form>
 
   <?php if(isset($_GET['submit'])) : 
     $nama = $_GET['nama'];
@@ -84,6 +97,15 @@
     </tbody>
   </table>
   <?php endif ?>
-</div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 bg-light pt-2 border border-2">
+        <p>Developed By Mahasiswa STT-NF</p>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/js/bootstrap.min.js" integrity="sha512-EKWWs1ZcA2ZY9lbLISPz8aGR2+L7JVYqBAYTq5AXgBkSjRSuQEGqWx8R1zAX16KdXPaCjOCaKE8MCpU0wcHlHA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 </html>
