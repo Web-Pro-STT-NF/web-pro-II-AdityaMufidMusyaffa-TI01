53ee78b649ac36d734bd0ad6d977eb9f3cae3ae6
adityamusyaffa <adit21030ti@student.nurulfikri.ac.id>
2022-04-14 17:27:22 +0700

    Chore: Do Task 3_class_mahasiswa

---
+++ praktikum_4/3_class_mahasiswa/1_daftar_predikat_mahasiswa/class_mahasiswa.php
@@ -0,0 +1,21 @@
+<?php
+class Mahasiswa {
+    public $nim;
+    public $nama;
+    public $thn_angkatan;
+    public $prodi;
+    public $ipk;
+    function __construct($nim, $nama, $thn_angkatan, $prodi, $ipk) {
+      $this->nim = $nim;
+      $this->nama = $nama;
+      $this->thn_angkatan = $thn_angkatan;
+      $this->prodi = $prodi;
+      $this->ipk = $ipk;
+    }
+    function predikat_ipk () {
+      if ($this->ipk < 2.0 ) return "Cukup";
+        elseif ($this->ipk >= 2.0 && $this->ipk < 3.0) return "Baik";
+        elseif ($this->ipk >= 3.0 && $this->ipk < 3.75) return "Memuaskan";
+        elseif ($this->ipk >= 3.75 && $this->ipk <= 4) return "Cum Laude";
+    }
+}
