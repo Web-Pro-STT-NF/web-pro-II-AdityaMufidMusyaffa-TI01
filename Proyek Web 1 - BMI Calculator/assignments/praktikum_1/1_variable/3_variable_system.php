e55b766b6942e3aa757d6b4e36a663d39deb39e8
adityamusyaffa <adit21030ti@student.nurulfikri.ac.id>
2022-04-23 23:17:40 +0700

    Update Rename

--- Proyek Web 1 - BMI Calculator/assignments/praktikum_1/1_variable/3_variable_system.php
+++
@@ -1,9 +0,0 @@
-<?php
-// variable system
-print_r($_SERVER);
-echo '<br>';
-echo '<br>';
-echo '<br>';
-echo 'Dokumen Root: '.$_SERVER["DOCUMENT_ROOT"];
-echo '<br/>Nama File: '.$_SERVER["PHP_SELF"];
-?>
--- Proyek Web 1 - BMI Calculator/assignments/proyek1_bmi/data_pasien.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'pasien.php';
-
-$pas1 = new Pasien('P001', 'Ahmad', 'Laki-Laki');
-$pas2 = new Pasien('P002', 'Rina', 'Perempuan');
-$pas3 = new Pasien('P003', 'Lutfi', 'Laki-Laki');
