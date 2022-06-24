3057c4f7926aee0c2c694d7cf50c2a822660d64d
adityamusyaffa <adit21030ti@student.nurulfikri.ac.id>
2022-06-24 20:42:49 +0700

    Add newDataMahasiswa() method in mahasiswa_model.php to catch new data

--- praktikum_7/application/models/Mahasiswa_model.php
+++ praktikum_7/application/models/Mahasiswa_model.php
@@ -3,4 +3,17 @@ class Mahasiswa_model extends CI_model {
   public function getAllMahasiswa() {
     return $this->db->get('mahasiswa')->result_array();
   }
+
+  public function newDataMahasiswa() {
+    $data = [
+      'nim' => $this->input->post('nim'),
+      'nama' => $this->input->post('nama'),
+      'gender' => $this->input->post('gender'),
+      'tmp_lahir' => $this->input->post('tmp_lahir'),
+      'tgl_lahir' => $this->input->post('tgl_lahir'),
+      'ipk' => $this->input->post('ipk'),
+      'kode_prodi' => $this->input->post('kode_prodi')
+    ];
+    return $data;
+  }
 }
