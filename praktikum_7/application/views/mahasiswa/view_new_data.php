0a884f270f9ca8a45faf73e49ab4d54cc1f2dd15
adityamusyaffa <adit21030ti@student.nurulfikri.ac.id>
2022-06-24 20:45:43 +0700

    Create view_new_data.php to display new data

---
+++ praktikum_7/application/views/mahasiswa/view_new_data.php
@@ -0,0 +1,7 @@
+<h6>NIM: <?= $mahasiswa['nim'] ?></h6>
+<h6>Nama: <?= $mahasiswa['nama'] ?></h6>
+<h6>gender: <?= $mahasiswa['gender'] ?></h6>
+<h6>tgl_mphir: <?= $mahasiswa['tmp_lahir'] ?></h6>
+<h6>tgl_lahir: <?= $mahasiswa['tgl_lahir'] ?></h6>
+<h6>IPK: <?= $mahasiswa['ipk'] ?></h6>
+<h6>kode_prodi: <?= $mahasiswa['kode_prodi'] ?></h6>
