0ea45832d14db2666659f0fb72eae3b906d5a8f5
adityamusyaffa <adit21030ti@student.nurulfikri.ac.id>
2022-06-24 17:40:47 +0700

    Add header and footer templates

---
+++ praktikum_7/application/views/templates/footer.php
@@ -0,0 +1,41 @@
+    <!-- /.content -->
+    </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="<?= base_url() ?>public/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="<?= base_url() ?>public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="<?= base_url() ?>public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url() ?>public/dist/js/adminlte.js"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+<script src="<?= base_url() ?>public/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="<?= base_url() ?>public/plugins/raphael/raphael.min.js"></script>
+<script src="<?= base_url() ?>public/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="<?= base_url() ?>public/plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<!-- ChartJS -->
+<script src="<?= base_url() ?>public/plugins/chart.js/Chart.min.js"></script>
+</body>
+</html>
